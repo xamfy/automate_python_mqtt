b4d3e6f7ca213b72dfea083a5bb4759bd57ed544
xamfy <spratyaksh22@gmail.com>
2019-01-29 17:52:58 +0530

    Added publish logic based on device status

--- script.py
+++ script.py
@@ -1,6 +1,7 @@
 import firebase_admin
 from firebase_admin import db
 from firebase_admin import credentials
+import paho.mqtt.publish as publish
 
 cred = credentials.Certificate(
     'serviceAccountKey.json')
@@ -11,10 +12,13 @@ firebase_admin = firebase_admin.initialize_app(
 def listener(event):
     # print(event.event_type)
     # print(event.path)
-    print(event.data)
-    # if(len(event.data) == 2):
-    # print(event.data['name'] + " " + str(event.data['status']))
     # print(event.data)
+    if(len(event.data) == 2):
+        print(event.data['name'] + " " + str(event.data['status']))
+        if(event.data['status'] == True):
+            publish.single("ON", event.data['name'], hostname="localhost")
+        elif(event.data['status'] == False):
+            publish.single("OFF", event.data['name'], hostname="localhost")
 
 
 db.reference('/devices').listen(listener)
